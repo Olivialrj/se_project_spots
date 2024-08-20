95f3f08da398d4f770b69e7efac29e009988d7cb
Olivialrj <olivialrj@hotmail.com>
2024-08-20 11:28:51 -0700

    New updates for final project 4

--- index.js
+++ index.js
@@ -25,16 +25,52 @@ const initialCards = [
   }),
 ];
 
+const profileNameElement = document.querySelector(".profile__name");
+const profileJobElement = document.querySelector(".profile__description");
 const profileEditButton = document.querySelector(".profile__button-secondary");
 const editModal = document.querySelector("#edit-modal");
 const editModalCloseButton = editModal.querySelector(".modal__button");
+const editFormElement = editModal.querySelector(".modal__form");
+const editNameInput = editModal.querySelector("#profile-name");
+const editJobInput = editModal.querySelector("#profile-description");
+const cardTemplate = document.querySelector("#card-template");
 
 function showModal() {
   editModal.classList.add("modal_opened");
+  editNameInput.value = profileNameElement.textContent;
+  editJobInput.value = profileJobElement.textContent;
 }
 
 function hideModal() {
   editModal.classList.remove("modal_opened");
 }
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileNameElement.textContent = editNameInput.value;
+  profileJobElement.textContent = editJobInput.value;
+
+  hideModal();
+}
+const cardTemplate = document.querySelector("#card-template");
+const cardList = document.querySelector(".card__list");
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card_caption");
+  cardNameEl.textContent = data.name;
+
+  return cardElement;
+}
+
 profileEditButton.addEventListener("click", showModal);
 editModalCloseButton.addEventListener("click", hideModal);
+editFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardList.prepend(cardElement);
+}
